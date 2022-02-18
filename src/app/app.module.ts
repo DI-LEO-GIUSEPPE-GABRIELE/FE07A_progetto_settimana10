2fb24f5de44791b362bceacbfcae235d5b51bd99
Jimbo9526 <giuseppegabrieledileo95@gmail.com>
2022-02-18 10:09:30 +0100

    primo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Route, RouterModule } from '@angular/router';
+import { TodoPage } from './pages/todo/todo.page';
+import { CompletedPage } from './pages/completed/completed.page';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+const routes:Route[] = [
+  {
+    path: '',
+    component: TodoPage
+  },
+  {
+    path: 'completed',
+    component: CompletedPage
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoPage,
+    CompletedPage,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
